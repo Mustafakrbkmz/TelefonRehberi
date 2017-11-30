e411b8ddfedd9602b2c1dd1b7b92bfd4c5d0fc93
Mustafa Karabekmez <karabekmezmustafa@gmail.com>
2017-11-30 18:33:17 +0300

    Master

---
+++ EntityFrameworkTelefonRehberi/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace EntityFrameworkTelefonRehberi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelNo = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.dgvTelefonlar = new System.Windows.Forms.DataGridView();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnBilgileriGetir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTelefonlar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnKaydet);
+            this.groupBox1.Controls.Add(this.txtTelNo);
+            this.groupBox1.Controls.Add(this.txtSoyad);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 151);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kaydetme Islemleri";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(105, 23);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(185, 26);
+            this.txtAd.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyad:";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(105, 52);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(185, 26);
+            this.txtSoyad.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tel No:";
+            // 
+            // txtTelNo
+            // 
+            this.txtTelNo.Location = new System.Drawing.Point(105, 82);
+            this.txtTelNo.Name = "txtTelNo";
+            this.txtTelNo.Size = new System.Drawing.Size(185, 26);
+            this.txtTelNo.TabIndex = 2;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(206, 118);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(105, 32);
+            this.btnKaydet.TabIndex = 3;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // dgvTelefonlar
+            // 
+            this.dgvTelefonlar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTelefonlar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvTelefonlar.Location = new System.Drawing.Point(0, 164);
+            this.dgvTelefonlar.Name = "dgvTelefonlar";
+            this.dgvTelefonlar.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvTelefonlar.Size = new System.Drawing.Size(622, 197);
+            this.dgvTelefonlar.TabIndex = 2;
+            this.dgvTelefonlar.RowLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTelefonlar_RowLeave);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(335, 125);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(98, 32);
+            this.btnSil.TabIndex = 0;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnBilgileriGetir
+            // 
+            this.btnBilgileriGetir.Location = new System.Drawing.Point(473, 126);
+            this.btnBilgileriGetir.Name = "btnBilgileriGetir";
+            this.btnBilgileriGetir.Size = new System.Drawing.Size(137, 32);
+            this.btnBilgileriGetir.TabIndex = 1;
+            this.btnBilgileriGetir.Text = "Bilgileri Getir";
+            this.btnBilgileriGetir.UseVisualStyleBackColor = true;
+            this.btnBilgileriGetir.Click += new System.EventHandler(this.btnBilgileriGetir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 361);
+            this.Controls.Add(this.btnBilgileriGetir);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.dgvTelefonlar);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTelefonlar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.TextBox txtTelNo;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvTelefonlar;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnBilgileriGetir;
+    }
+}
+
---
+++ EntityFrameworkTelefonRehberi/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkTelefonRehberi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TelRehberiDBEntities db = new TelRehberiDBEntities();
+        private void btnBilgileriGetir_Click(object sender, EventArgs e)
+        {
+            VerileriCek();
+        }
+
+        private void VerileriCek()
+        {
+            dgvTelefonlar.DataSource = db.Kisiler.ToList();
+            #region Kisiidyi getirmeme
+            //dgvTelefonlar.DataSource = db.Kisiler.Select(k => new
+            //{
+            //    k.KisiAdi,
+            //    k.KisiSoyadi,
+            //    k.TelefonNo
+            //}).ToList(); 
+            #endregion
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            //Mustafa Karabekmez
+            foreach (Control item in groupBox1.Controls)
+            {
+                if (item is TextBox)
+                {
+                    TextBox txt = (TextBox)item;
+                    if (string.IsNullOrEmpty(txt.Text))
+                    {
+                        MessageBox.Show("Lutfen tum bilgileri giriniz.");
+                        return;
+                    }
+                }
+            }
+
+            //1.Yontem
+            //Kisiler yeniKisi = new Kisiler();
+            //yeniKisi.KisiAdi = txtAd.Text;
+            //yeniKisi.KisiSoyadi = txtSoyad.Text;
+            //yeniKisi.TelefonNo = txtTelNo.Text;
+            //db.Kisiler.Add(yeniKisi);
+            //db.SaveChanges();
+            //VerileriCek();
+
+            //2.Yontem
+            //Kisiler yeniKisi = new Kisiler
+            //{
+            //    KisiAdi = txtAd.Text,
+            //    KisiSoyadi = txtSoyad.Text,
+            //    TelefonNo = txtTelNo.Text
+            //};
+            //db.Kisiler.Add(yeniKisi);
+            //db.SaveChanges();
+            //VerileriCek();
+
+            //3.Yontem
+            db.Kisiler.Add(new Kisiler
+            {
+                KisiAdi = txtAd.Text,
+                KisiSoyadi = txtSoyad.Text,
+                TelefonNo = txtTelNo.Text
+            });
+            db.SaveChanges();
+            VerileriCek();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (dgvTelefonlar.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dgvTelefonlar.SelectedRows[0].Cells[0].Value);
+                //Kisiler silinecek = db.Kisiler.FirstOrDefault(kisi => kisi.KisiID == id);
+
+                //Kisiler silinecek = db.Kisiler.Find(id);
+
+                //db.Kisiler.Remove(silinecek);
+                //db.SaveChanges();
+                //VerileriCek();
+
+                db.Kisiler.Remove(db.Kisiler.Find(id));
+                db.SaveChanges();
+                VerileriCek();
+            }
+        }
+
+        private void dgvTelefonlar_RowLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            db.SaveChanges();
+        }
+    }
+}
